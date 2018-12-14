9b1c0aff6d9935ca6193ec0450a735020df02ff3
Juan Pablo Gonzalez <jp.gonzalez14@uniandes.edu.co>
2018-12-13 20:59:24 -0500

    quote api docs

--- routes/api/quotes.js
+++ routes/api/quotes.js
@@ -6,4 +6,24 @@ const router = express.Router();
 //@access Public
 router.get('/', (req,res) => res.json({quote: "this is a quote"}));
 
+//@route  POST api/quotes/create
+//@desc   create quote
+//@access Public
+
+
+//@route  GET api/quotes/quotes
+//@desc   get a lis of quotes
+//@access Public
+
+
+//@route  PUT api/quotes/edit
+//@desc   edit an existing quote
+//@access Private
+
+
+//@route  DELETE api/quotes/delete
+//@desc   delete existing quote
+//@access Private
+
+
 module.exports = router;
